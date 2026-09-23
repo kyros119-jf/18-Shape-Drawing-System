983148b2d1c8eabc82e544e09e3ef93771cb860c
Jeremy <jeremy.frank@students.snow.edu>
2026-09-23 10:45:46 -0600

    Complete shape drawing system assignment

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Shape shape1 = new Circle();
+Shape shape2 = new Rectangle();
+Shape shape3 = new Triangle();
+
+
+shape1.Draw(); // Output: Drawing a Circle
+Console.WriteLine();
+shape2.Draw(); // Output: Drawing a Rectangle
+Console.WriteLine();
+shape3.Draw(); // Output: Drawing a Triangle
---
+++ Shape.cs
@@ -0,0 +1,31 @@
+﻿public abstract class Shape
+{
+
+    public abstract void Draw();
+
+}
+
+public class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a Circle");
+    }
+}
+
+public class Rectangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a Rectangle");
+    }
+}
+
+public class Triangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a Triangle");
+    }
+}
+
